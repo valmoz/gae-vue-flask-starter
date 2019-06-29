c7e4e8540b2cf8052036497f9f634ae643c5d33a
m.valota <m.valota@goreply.io>
2019-06-29 21:08:41 +0200

    Updated to Python 3.7 SDK.

--- main.py
+++ main.py
@@ -1,25 +1,56 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START gae_python37_app]
+from flask import Flask, render_template
+
 import logging
 import jinja2
 import os
-import urllib2
+import urllib.request
 
-from flask import Flask, render_template
-
+# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# called `app` in `main.py`.
 app = Flask(__name__)
 app.jinja_loader = jinja2.FileSystemLoader('app/dist')
 
 
+@app.route('/hello')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def vue_client(path):
-    if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
+    if os.getenv('GAE_ENV', '').startswith('standard'):
         return render_template('index.html')
     else:
         url = 'http://localhost:3000/{0}'.format(path)
-        return urllib2.urlopen(url).read()
+        logging.error(url)
+        return urllib.request.urlopen(url).read()
 
 
 @app.errorhandler(500)
 def server_error(e):
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
+
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
+# [END gae_python37_app]
